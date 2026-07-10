c181b228d9b65d7305fffba45bf4b3b9b8921814
Yusuf Akdogan <a.akdogan101@gmail.com>
2026-07-10 18:47:34 +0300

    spring projects for session and station services are initialized

---
+++ session-service/src/main/java/com/yusufakdogan/session_service/SessionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yusufakdogan.session_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionServiceApplication.class, args);
+	}
+
+}
